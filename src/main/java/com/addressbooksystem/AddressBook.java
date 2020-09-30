3cfffa07d96a944f39d5309bb6820bb51586b4ec
Akhil-star <akhilakki1305@gmail.com>
2020-09-30 16:30:47 +0530

    Contact added

---
+++ src/main/java/com/addressbooksystem/AddressBook.java
@@ -0,0 +1,21 @@
+package com.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+	
+	private ArrayList<Contact> addressBook=new ArrayList<Contact>();
+    
+    public void setAddressBook(ArrayList<Contact> obj)
+    {
+   	 addressBook=obj;
+    }
+    public ArrayList<Contact> getAddressBook()
+    {
+   	 return addressBook;
+    }
+    public void addContact(Contact contactObj)
+    {
+   	 addressBook.add(contactObj);
+    }
+}
