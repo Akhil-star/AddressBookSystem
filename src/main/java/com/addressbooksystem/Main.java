c26d7dd1365550616cf0cbab0a29e9dac645d84f
Akhil-star <akhilakki1305@gmail.com>
2020-09-30 22:30:44 +0530

    Merge2

--- src/main/java/com/addressbooksystem/Main.java
+++ src/main/java/com/addressbooksystem/Main.java
@@ -13,9 +13,32 @@ public class Main
     {
     	Scanner sc=new Scanner(System.in);
 		AddressBook objmain=new AddressBook();
-		System.out.println("2.Edit");
+		System.out.println("1.Add\n2.Edit");
 		System.out.println("Enter your choice");
 		int n=sc.nextInt();
+		if(n==1)
+		{
+			Contact obj=new Contact();
+			System.out.println("Add a contact");
+			System.out.println("Enter the first name :");
+			obj.setFirstName(sc.nextLine());
+			sc.nextLine();
+			System.out.println("Enter the last name :");
+            obj.setLastName(sc.nextLine());
+			System.out.println("Enter the city :");
+			obj.setCity(sc.nextLine());
+			System.out.println("Enter the state :");
+			obj.setState(sc.nextLine());
+			System.out.println("Enter the zip :");
+			obj.setZip(Long.parseLong(sc.nextLine()));
+			System.out.println("Enter the phone no :");
+			obj.setPhoneNumber(sc.nextLong());
+			System.out.println("Enter the emailid :");
+			obj.setEmailId(sc.nextLine());
+			sc.nextLine();
+			objmain.addContact(obj);
+            System.out.println( "Contact addded" );
+		}
 		if(n==2)
 		{
 			System.out.println("Enter the First name : ");
